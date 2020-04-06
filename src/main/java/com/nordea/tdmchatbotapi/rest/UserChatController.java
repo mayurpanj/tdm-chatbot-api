7d4d775c16673a7b20727124ca8887f77db25272
Mayur Panjwani <Mayur Panjwani@192.168.1.204>
2020-04-06 15:43:51 +0530

    tdm-chatbot-api: bug fixes and addition of few more questions

--- src/main/java/com/nordea/tdmchatbotapi/rest/UserChatController.java
+++ src/main/java/com/nordea/tdmchatbotapi/rest/UserChatController.java
@@ -1,15 +1,18 @@
 package com.nordea.tdmchatbotapi.rest;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 
 import org.alicebot.ab.Bot;
 import org.alicebot.ab.MagicBooleans;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.HandlerMapping;
 
 import com.nordea.tdmchatbotapi.model.QueryResult;
 import com.nordea.tdmchatbotapi.service.UserChatService;
@@ -26,10 +29,22 @@ public class UserChatController {
 	private UserChatService userChatService;
 
 	// Example: http://localhost:8090/chatresponse/"how to get production data"
-	@RequestMapping("/chatresponse/{query}")
-	public QueryResult getQueryResponse(@PathVariable String query) {
-		LOG.info("The request sent by the user is:" + query);
-		return userChatService.getQueryResponse(query);
+	@RequestMapping("/chatresponse/{query}/**")
+	public QueryResult getQueryResponse(@PathVariable String query, HttpServletRequest request) {
+		final String path = request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE).toString();
+		final String bestMatchingPattern = request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE).toString();
+
+	    String arguments = new AntPathMatcher().extractPathWithinPattern(bestMatchingPattern, path);
+
+	    String userQuery;
+	    if (null != arguments && !arguments.isEmpty()) {
+	    	userQuery = query + '/' + arguments;
+	    } else {
+	    	userQuery = query;
+	    }
+	    
+		LOG.info("The request sent by the user is:" + userQuery);
+		return userChatService.getQueryResponse(userQuery);
 	}
 
 	@PostConstruct
